cd3f2c3960294c22bd4cc3bd773c13761a73a6f9
HP <sreekanta.csd@gmail.com>
2022-09-16 10:10:48 +0600

    search box added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,13 +4,26 @@ import Activity from '../Activity/Activity';
 
 const Main = () => {
   const [volunteers, setVolunteers] = useVolunteers()
+  const [searchResult, setSearchResult] = useState([])
+
+  const handleSearchChange= event=>{
+    const searchText = event.target.value;
+    const match = volunteers.filter(v => v.title.toLowerCase().includes(searchText))
+    setSearchResult(match)
+  }
     
     return (
-        <div className='grid-cols-1 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 p-4 mt-12 '>
+
+      <div>
+        <div>
+            <input onChange={handleSearchChange} className='p-3 border-2 border-orange-400 m-4' type="text" placeholder='Search'  />
+        </div>
+          <div className='grid-cols-1 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 p-4 mt-12 '>
            {
-            volunteers.map(volunteer => <Activity key={volunteer._id} volunteer={volunteer}></Activity>)
+            searchResult.map(volunteer => <Activity key={volunteer._id} volunteer={volunteer}></Activity>)
            }
         </div>
+      </div>
     );
 };
 
